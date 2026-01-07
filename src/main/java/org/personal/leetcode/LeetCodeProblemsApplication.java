a9ca69832a47ece486d1d294acf9eda15a44be0a
Krishnaprasad <krishna.arakkal@gmail.com>
2026-01-07 23:44:58 +0530

    Initial commit

---
+++ src/main/java/org/personal/leetcode/LeetCodeProblemsApplication.java
@@ -0,0 +1,13 @@
+package org.personal.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeProblemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeProblemsApplication.class, args);
+    }
+
+}
